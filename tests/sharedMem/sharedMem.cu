d1bac090ba7c60154dc886b4cc4f2bd9922645c0
Bin Wu <bin.wu.learn@gmail.com>
2021-06-25 22:57:00 +0200

    First run on Alienware!

--- tests/sharedMem/sharedMem.cu
+++ tests/sharedMem/sharedMem.cu
@@ -4,11 +4,11 @@ using namespace std;
 #include <macros.h>
 #include <CompTime.h>
 
-#define DIMBLK 128
-#define DIMGRD 1024
+#define DIMGRD 512
+#define DIMBLK 1024
 
 __device__ __host__ float fab(float a, float b){
-  return a*b;
+  return a*b*a*b;
 }
 
 __global__ void sum_global(float *a_d, float *b_d, float *out_d){
@@ -17,24 +17,22 @@ __global__ void sum_global(float *a_d, float *b_d, float *out_d){
 }
 
 int main(){
-  cout << "Let us start!" << endl;
   int dim = DIMBLK*DIMGRD;
-  cout << dim << endl;
-  float *a_d, *b_d, *out_d;
+    float *a_d, *b_d, *out_d;
   float a_h[dim], b_h[dim], out_h[dim];
   for(int i=0; i<dim; i++){
     a_h[i] = (float) i; b_h[i] = (float) i;
   }
-  cout << "a_h and b_h has just been initialized!" << endl;
+  //cout << "a_h and b_h has just been initialized!" << endl;
   
   CompTime stopwatch;
   for(int i=0; i<dim; i++){
     out_h[i] = fab(a_h[i], b_h[i]);
   }
 
-  cout << "Computation time: " << stopwatch.getTime() << " s." << endl;
+  cout << "CPU computation time: " << scientific << stopwatch.getTime() << " s.\n" << endl;
     
-  cout << "a_h and b_h has just been initialized!" << endl;
+  //cout << "a_h and b_h has just been initialized!" << endl;
 
   size_t _nbytes = dim * sizeof(float);
   CUDA_STATUS(cudaMalloc((void**) &a_d, _nbytes));
@@ -50,7 +48,7 @@ int main(){
     fprintf( stderr , "Error %s at line %d in file %s \n", cudaGetErrorString(err), __LINE__, __FILE__);
     exit(1);
   }
-  cout << "Computation time: " << stopwatch.getTime() << " s." << endl;
+  cout << "GPU with globalMem computation time with (" << DIMGRD << ", " << DIMBLK << "): " << stopwatch.getTime() << " s.\n" << endl;
 
 
   cudaFree((void*) a_d);
