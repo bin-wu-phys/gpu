0e1eeac19b6d175997529a71f60d53a1114269c8
Bin Wu <bin.wu.learn@gmail.com>
2021-06-29 13:20:31 +0200

    Finalize on streamomp. Strange behavior for text modes found for computation time.

--- tests/streams/naive.cpp
+++ tests/streams/naive.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+#include <omp.h>
 #include "CompTime.h"
 #include "StreamNaive.cuh"
 
@@ -11,10 +12,11 @@ int main(){
   float dt = sn.stop();
   cout << "Computation time by using events: " << dt << " ms." << endl;
 
-  CompTime stopwatch;
+  //CompTime stopwatch;
+  double wt = omp_get_wtime();
   sn.run();
   sn.setSync(deviceSync);
-  cout << "Computation time by cpu time: " << stopwatch.getTime()*1e3 << " ms." << endl;
+  cout << "Computation time by cpu time: " << (omp_get_wtime() - wt)*1e3 << " ms." << endl;
 
   cout << "Existing the host!\n";
   return 0;
